d2c85d175d0d3d3125d05e1289d66799e1b7bd55
Harivignesh <harivignesh.r@dsrc.co.in>
2018-09-21 17:54:40 +0530

    Https

--- spec.js
+++ spec.js
@@ -2,7 +2,7 @@ browser.ignoreSynchronization = true;
 
 describe('Protractor Demo App', function() {
     it('should have a title', function() {
-      browser.get('http://juliemr.github.io/protractor-demo/');
+      browser.get('https://juliemr.github.io/protractor-demo/');
   
       expect(browser.getTitle()).toEqual('Super Calculator');
     });
@@ -10,7 +10,7 @@ describe('Protractor Demo App', function() {
 
   describe('Protractor Demo App', function() {
     it('should add one and two', function() {
-      browser.get('http://juliemr.github.io/protractor-demo/');
+      browser.get('https://juliemr.github.io/protractor-demo/');
       element(by.model('first')).sendKeys(1);
       element(by.model('second')).sendKeys(2);  
       element(by.id('gobutton')).click();        

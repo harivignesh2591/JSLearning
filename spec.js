ddcc2b29301998b7c322897075c45ddf5107edcc
Harivignesh <harivignesh.r@dsrc.co.in>
2018-09-21 17:56:57 +0530

    Sleep added

--- spec.js
+++ spec.js
@@ -13,7 +13,8 @@ describe('Protractor Demo App', function() {
       browser.get('https://juliemr.github.io/protractor-demo/');
       element(by.model('first')).sendKeys(1);
       element(by.model('second')).sendKeys(2);  
-      element(by.id('gobutton')).click();        
+      element(by.id('gobutton')).click(); 
+      await sleep(3000);        
       expect(element(by.binding('latest')).getText()).
           toEqual('3'); // This is wrong!
           console.log(element(by.binding('latest')).getText())

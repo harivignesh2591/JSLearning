287c39aa7ae39eede7be47eb5bc1cd4c9fcced25
Harivignesh <harivignesh.r@dsrc.co.in>
2018-09-24 14:27:09 +0530

    Changes on the TOBE

--- spec.js
+++ spec.js
@@ -1,27 +1,35 @@
 browser.ignoreSynchronization = true;
 
-describe('Protractor Demo App', function() {
-    it('should have a title', function() {
-      browser.get('https://juliemr.github.io/protractor-demo/');
-  
-      expect(browser.getTitle()).toEqual('Super Calculator');
-    });
-  });
+var Thread = {
+	sleep: function(ms) {
+		var start = Date.now();
+		
+		while (true) {
+			var clock = (Date.now() - start);
+			if (clock >= ms) break;
+		}
+		
+	}
+};
+
+// describe('Protractor Demo App', function() {
+//     it('should have a title', function() {
+//       browser.get('https://juliemr.github.io/protractor-demo/');      
+//       expect(browser.getTitle()).toEqual('Super Calculator');
+//     });
+//   });
 
   describe('Protractor Demo App', function() {
     it('should add one and two', function() {
-      browser.get('https://juliemr.github.io/protractor-demo/');
-      await.sleep(2000);
+      browser.get('https://juliemr.github.io/protractor-demo/');      
       element(by.model('first')).sendKeys(1);
       element(by.model('second')).sendKeys(2);  
       element(by.id('gobutton')).click(); 
-     
+      browser.sleep(5000);
+      element(by.binding())
+      Thread.sleep(5000);
       console.log(element(by.binding('latest')).getText());
-      expect(element(by.binding('latest')).getText()).toEqual('3'); // This is wrong!
+      expect(element(by.binding('latest')).getText()).toBe('3'); // This is wrong!
           
     });
   });
-
-
-
-  
